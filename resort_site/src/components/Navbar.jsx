0cbd0705e51e2a7c2684f87f0926ae5ad5f26a6b
Brian Lu <45515803+bl2456@users.noreply.github.com>
2020-12-06 17:53:33 -0500

    fixed links for github pages

--- resort_site/src/components/Navbar.jsx
+++ resort_site/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
     //https://react-bootstrap.github.io/components/navbar/
     return(
         <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
-            <Navbar.Brand href="/">
+            <Navbar.Brand href="#/">
                 <FaHotel className='nav-icon' />
                 <span className='brand-name'>
                     Tokyo Night Resort
@@ -18,11 +18,11 @@ const NavBar = () => {
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/#/Rooms">Rooms</Nav.Link>
+                    <Nav.Link href="#/">Home</Nav.Link>
+                    <Nav.Link href="#/rooms">Rooms</Nav.Link>
                 </Nav>
                 <Nav>
-                    <Nav.Link href="#deets">About Us</Nav.Link>
+                    <Nav.Link href="#/about">About Us</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
